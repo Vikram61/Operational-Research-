ca5066710f90c9e3b502b1b7b2efb290f56c702b
Masana Vikram Vasisht <134063129+Vikram61@users.noreply.github.com>
2025-05-02 10:58:40 +0530

    Update app.py

--- simplex/app.py
+++ simplex/app.py
@@ -4,6 +4,9 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+@app.route("/")
+def home():
+    return "Simplex API is running"
 
 @app.route("/simplex", methods=["POST"])
 def simplex_method():
